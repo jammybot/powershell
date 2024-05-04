4b665a14168c837c9409bea9421927b9f6f38056
jammybot <59058624+jammybot@users.noreply.github.com>
2024-05-04 17:17:22 +0100

    Add files via upload

--- version-checker.ps1
+++ version-checker.ps1
@@ -1,12 +1,17 @@
 # Install-Module -Name ImportExcel -Scope CurrentUser
 # winget install winget -a
 # Install-Module -Name Microsoft.PowerShell.Winget -Scope CurrentUser
-
+#
 # Import required modules
 Import-Module ImportExcel
 
+# Define log file path
+$UptoDateFilePath = "./UptoDateLog.txt"
+$NoMatchFilePath = "./NoMatchLog.txt"
+$NewUpdateFilePath ="./NewUpdateLog.txt"
+
 # Step 2: Read software names from Excel file
-$excelFilePath = "E:\Uni\scripts\Book1.xlsx"
+$excelFilePath = "E:\Uni\scripts\Book2.xlsx"
 $softwareList = Import-Excel -Path $excelFilePath -WorksheetName "Sheet1" -HeaderRow 1
 
 # Step 3-6: Iterate through each software entry, search in Winget, compare versions, and update Excel
@@ -24,18 +29,19 @@ foreach ($softwareEntry in $softwareList) {
     if ($wingetSearchResult) {
         # For loop to compare
         foreach ($result in $wingetSearchResult) {
+            $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
             $latestVersion = $result.Version
             # Compare versions
             if ($latestVersion -eq $currentVersion) {
-                Write-Output "$softwareName is up to date."
+                Write-Output "$timestamp - $softwareName is up to date." | Out-File -FilePath $UptoDateFilePath -Append
             } else {
-                Write-Output "$softwareName is not up to date. Current version: $softwareVersion, Latest version: $latestVersion"
+                Write-Output "$timestamp - $softwareName is not up to date. Current version: $softwareVersion, Latest version: $latestVersion" | Out-File -FilePath $NewUpdateFilePath -Append
                 # Update Excel with latest version
                 $softwareEntry.LatestVersion = $latestVersion
                 Export-Excel -Path $excelFilePath -WorksheetName "Sheet1" -AutoSize -InputObject $softwareList -ClearSheet 
             }
         }
     } else {
-        Write-Host "No matching software found for '$softwareName'."
+        Write-Output "$timestamp - No matching software found for '$softwareName'." | Out-File -FilePath $NoMatchFilePath -Append
     }
 }
